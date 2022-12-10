0a73f8ef5d9abb9eb615d910489f0e872bf20a23
elirehema <rocksandystone@gmail.com>
2022-12-10 17:26:36 +0300

    Update GH endpoint

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const routerBase = process.env.DEPLOY_ENV === 'prod' ? '/' : '/';
+const routerBase = process.env.DEPLOY_ENV === 'prod' ? '/' : '/subway';
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
