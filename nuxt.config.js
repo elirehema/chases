4d99330cfc8173f70825eec5a30bf6b67437df4c
elirehema <rocksandystone@gmail.com>
2022-12-10 17:34:53 +0300

    Update GH endpoint

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    publicPath: process.env.NODE_ENV === 'production' ? '/assets/' : '',
+    publicPath: process.env.NODE_ENV === 'prod' ? '/subway/' : '/subway/',
     extend(config, ctx) { },
     postcss: {
       preset: {
