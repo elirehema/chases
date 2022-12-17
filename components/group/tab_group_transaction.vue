b027c6fadbf4102c08338bf2e2cc44b486ca8021
elirehema <rocksandystone@gmail.com>
2022-12-17 13:36:01 +0300

    Add grou services

---
+++ components/group/tab_group_transaction.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-data-table
+    v-if="transactions"
+    :headers="headers"
+    :items="transactions"
+    sort-by="calories"
+    class="elevation-1"
+  >
+    <template #top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title class="font-weight-bold text-h3">
+          Group transactions
+        </v-toolbar-title>
+        <v-spacer />
+      </v-toolbar>
+    </template>
+
+    <template #no-data>
+      <p>No data</p>
+    </template>
+  </v-data-table>
+  <skeleton-table-loader v-else />
+</template>
+<script>
+export default {
+  props: {
+    transactions: {
+      type: Array,
+      default: null
+    }
+  },
+  data: () => ({
+    headers: [
+      {
+        text: 'Service',
+        align: 'start',
+        sortable: false,
+        value: 'serviceName'
+      },
+
+      { text: 'Group ID', value: 'groupId' },
+      { text: 'MSISDN', value: 'msisdn' },
+      { text: 'Receipt #', value: 'receipt' },
+
+      { text: 'Balance Before', value: 'balanceBefore' },
+      { text: 'Balance After', value: 'balanceAfter' },
+      { text: 'Pay Number', value: 'payNumber' },
+      { text: 'Bank Account', value: 'bankAccount' },
+
+      { text: 'Payment Ref', value: 'paymentReference' }
+    ]
+  })
+}
+</script>
