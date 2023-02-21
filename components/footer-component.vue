0f0aaad0a1b912371fd4f90916b59d69ac459a7e
Elirehema Paul <rocksandystone@gmail.com>
2023-02-22 02:15:37 +0300

    Rename copyrighhts

--- components/footer-component.vue
+++ components/footer-component.vue
@@ -3,7 +3,7 @@
     <v-footer app>
       <v-spacer />
       <span class="mr-8">Version: 22.04.24.0596</span>
-      <span>CopyRight &copy; {{ new Date().getFullYear() }} Nexis Africa Group</span>
+      <span>CopyRight &copy; {{ new Date().getFullYear() }} Ngazi Technologies</span>
     </v-footer>
   </v-container>
 </template>
